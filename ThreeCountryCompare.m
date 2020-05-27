8f5f714449698f96d55da6b9575604682260722a
Christopher Stapels <cstapels@hotmail.com>
2020-05-27 14:21:18 -0400

    Update ThreeCountryCompare.m

--- ThreeCountryCompare.m
+++ ThreeCountryCompare.m
@@ -1,7 +1,25 @@
+% Enter your MATLAB code below
 covidChannelID = 1066064;
 myData = thingSpeakRead(covidChannelID,'outputFormat','timetable','numDays',300);
 
-plot(myData.Timestamps,myData.USA,myData.Timestamps,...
-    myData.China,myData.Timestamps,myData.Spain);
-ylabel('Confirmed Covid Cases');
-legend('USA','China','Spain','Location','NorthWest');
+sequenceUS = diff(myData.USA);
+sequenceChina = diff(myData.China);
+sequenceUK = diff(myData.UK);
+
+sequenceTimes = myData.Timestamps(1:end-1);
+
+plot(sequenceTimes, sequenceUS,sequenceTimes,sequenceUK,sequenceTimes,sequenceChina);
+
+ylabel('Daily New Cases');
+
+ylim([0 4E4]);
+
+legend('USA','China','UK','Location','West');
+
+smoothedUS=movmean(sequenceUS,20);
+yyaxis right;
+plot(sequenceTimes,smoothedUS);
+legend('Smoothed US Data','Location','West');
+
+threshold = 0.8 * max(smoothedUS);
+yline(threshold);
